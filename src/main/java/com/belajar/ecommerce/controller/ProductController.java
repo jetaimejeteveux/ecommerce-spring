88d7b961e11a34e2bc4a72ebc32dd7421a0e841e
jetaimejeteveux <akufirman22@gmail.com>
2025-01-20 23:27:20 +0700

    feat: implement product service in product controller for CRUD operations

--- src/main/java/com/belajar/ecommerce/controller/ProductController.java
+++ src/main/java/com/belajar/ecommerce/controller/ProductController.java
@@ -5,16 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.belajar.ecommerce.model.ErrorResponse;
 import com.belajar.ecommerce.model.ProductRequest;
 import com.belajar.ecommerce.model.ProductResponse;
+import com.belajar.ecommerce.service.ProductService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,75 +33,43 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping("products")
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final ProductService productService;
+
     @GetMapping("/{id}")
-    public ResponseEntity<ProductResponse> findProductById(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(
-            ProductResponse.builder()
-            .name("product" + id)
-            .description("product description")
-            .price(BigDecimal.ONE)
-            .build()
-        );
+    public ResponseEntity<ProductResponse> findProductById(@PathVariable("id") Long productId) {
+        ProductResponse productResponse = productService.findByid(productId);
+        return ResponseEntity.ok(productResponse);
     }
 
     @GetMapping("")
     public ResponseEntity<List<ProductResponse>> getAllProducts() {
-        return ResponseEntity.ok(
-            List.of(
-                ProductResponse.builder()
-                .name("product 1")
-                .price(BigDecimal.ONE)
-                .description("product 1 description")
-                .build(),
-                ProductResponse.builder()
-                .name("product 2")
-                .price(BigDecimal.TEN)
-                .description("product 2 description")
-                .build()
-            ));
+        List<ProductResponse> productResponses = productService.findAll();
+        return ResponseEntity.ok(productResponses);
     }
 
     @PostMapping("")
     public ResponseEntity<ProductResponse> createProduct(@RequestBody @Valid ProductRequest productRequest) {
-         return ResponseEntity.ok(
-            ProductResponse.builder()
-            .name(productRequest.getName())
-            .description(productRequest.getDescription())
-            .price(BigDecimal.ONE)
-            .build()
-        );
+        ProductResponse productResponse = productService.create(productRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ProductResponse> updateProduct(
         @RequestBody @Valid ProductRequest productRequest, 
-        @PathVariable Long id 
+        @PathVariable("id") Long productId
     ) {
-         return ResponseEntity.ok(
-            ProductResponse.builder()
-            .name(productRequest.getName())
-            .description(productRequest.getDescription())
-            .price(BigDecimal.ONE)
-            .build()
-        );
+        ProductResponse productResponse = productService.update(productRequest, productId);
+        return ResponseEntity.ok(productResponse);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public @ResponseBody ErrorResponse handleValidationException(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach(objectError -> {
-        String fieldName = ((FieldError) objectError).getField();
-        String errorMessage = objectError.getDefaultMessage();
-        errors.put(fieldName, errorMessage);
-        });
-        return ErrorResponse.builder()
-            .code(HttpStatus.BAD_REQUEST.value())
-            .message(errors.toString())
-            .timestamp(LocalDateTime.now())
-            .build();
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(
+        @PathVariable(name = "id") Long productID
+    ) {
+        productService.delete(productID);
+        return ResponseEntity.noContent().build();
     }
-    
 }
--- src/main/resources/db/migration/V1__create_new_product_and_categories.sql
+++ src/main/resources/db/migration/V1__create_new_product_and_categories.sql
@@ -4,6 +4,7 @@ CREATE TABLE product (
     description TEXT,
     price DECIMAL(10, 2) NOT NULL,
     stock_quantity INT NOT NULL,
+    weight DECIMAL(10, 2) NOT NULL DEFAULT 1000.0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL
 );
